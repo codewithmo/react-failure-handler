f667b03ca7ade3e3aaab3ab724190643221c1881
codewithmo <mlmypro1@gmail.com>
2022-10-09 01:18:48 +0530

    router and exp-collapse ready

--- src/components/MessageDetails.jsx
+++ src/components/MessageDetails.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Box from "react-styled-box";
+import { BiChevronDown } from "react-icons/bi";
+import useCollapse from "react-collapsed";
 
 const MessageDetails = () => {
   let { id } = useParams();
   let [messageId, setMessageId] = useState("");
   let [messageDetails, setMessageDetails] = useState({});
+  const [isExpanded, setExpanded] = useState(false);
+  const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded });
   useEffect(() => {
     axios
       .get(`/failure-messages/${id}`, {
@@ -25,7 +30,39 @@ const MessageDetails = () => {
   return (
     <div>
       <div>Message details of id: {messageId}</div>
-      <pre>{JSON.stringify(messageDetails)}</pre>
+
+      <Box flexDirection="column" width="100%">
+        <Box className="item-box">
+          <Box flexDirection="row" justifyContent="space-between">
+            <Box className="box-btn center-content">
+              {messageDetails.createdAt?.split("T")[0]}
+            </Box>
+            <Box className="box-btn center-content">{messageDetails.label}</Box>
+            <Box className="box-btn center-content">{messageDetails.id}</Box>
+            <Box className="box-btn center-content">
+              {messageDetails.status}
+            </Box>
+          </Box>
+          <Box>
+            <Box className="center-content icon-down-box">
+              <BiChevronDown
+                {...getToggleProps({
+                  onClick: () => setExpanded((prevExpanded) => !prevExpanded),
+                })}
+              >
+                {isExpanded ? "Collapse" : "Expand"}
+              </BiChevronDown>
+            </Box>
+          </Box>
+        </Box>
+        <Box>
+          <section {...getCollapseProps()}>
+            <Box className="exp-coll-box center-content">
+              <pre>{JSON.stringify(messageDetails, null, 2)}</pre>
+            </Box>
+          </section>
+        </Box>
+      </Box>
     </div>
   );
 };
